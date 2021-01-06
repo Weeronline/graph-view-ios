aee9e45cbcd527fd6c754afbc834005b2ee2909f
Eduardo Irias <e.irias@weeronline.nl>
2021-01-06 11:57:42 +0100

    Add Vertical Lines
    Add temporal vertical lines with temporal color

--- Sources/GraphView/GraphView.swift
+++ Sources/GraphView/GraphView.swift
@@ -1,5 +1,42 @@
 import UIKit
 
 class GraphView: UIView {
-    var text = "Hello, World!"
+    
+    var isScrolling = false
+    var barWidth: CGFloat = 20
+    
+    private var temporalNumberOfItems = 10
+    private var temportalColorForVerticalLine: UIColor? = UIColor.red
+    
+    override func draw(_ rect: CGRect) {
+        guard let context = UIGraphicsGetCurrentContext() else {
+          return
+        }
+                
+        addGraphVerticalLines(size: rect.size)
+       
+        context.saveGState()
+
+    }
+    
+    // MARK: - Drawing
+    
+    private func addGraphVerticalLines(size: CGSize) {
+        for index in 0...temporalNumberOfItems {
+            guard let verticalLineColor = temportalColorForVerticalLine else {
+                continue
+            }
+
+            let linePath = UIBezierPath()
+
+            let xAxis = barWidth * CGFloat(index)
+            linePath.move(to: CGPoint(x: xAxis, y: 0))
+            linePath.addLine(to: CGPoint(x: xAxis, y: size.height))
+            
+            verticalLineColor.setStroke()
+            linePath.lineWidth = 1.0
+            linePath.stroke()
+        }
+    }
+    
 }
