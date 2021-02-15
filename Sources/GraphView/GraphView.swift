1b9f729ab2dce00a46c64a0287dde5f9ba47faa7
Eduardo Irías <eduardoirias@me.com>
2021-02-15 16:07:46 +0100

    Add Horizontal Lines

--- Sources/GraphView/GraphView.swift
+++ Sources/GraphView/GraphView.swift
@@ -8,13 +8,16 @@ public class GraphView: UIView {
     
     private var temporalNumberOfItems = 10
     private var temportalColorForVerticalLine: UIColor? = UIColor.red
-    
+    private let temportalHorizontalLines: [CGFloat] = [0.0, 0.04, 0.2, 0.5, 0.88, 0.9, 1.0]
+
     public override func draw(_ rect: CGRect) {
         guard let context = UIGraphicsGetCurrentContext() else {
           return
         }
-                
+        
         addGraphVerticalLines(size: rect.size)
+        addGraphHorizontalLines(size: rect.size)
+        
        
         context.saveGState()
     }
@@ -34,6 +37,21 @@ public class GraphView: UIView {
             linePath.addLine(to: CGPoint(x: xAxis, y: size.height))
             
             verticalLineColor.setStroke()
+            linePath.lineWidth = 1.0
+            linePath.stroke()
+        }
+    }
+    
+    private func addGraphHorizontalLines(size: CGSize) {
+        for index in 0..<temportalHorizontalLines.count {
+            let linePath = UIBezierPath()
+            
+            let line = temportalHorizontalLines[index]
+            let yAxis = size.height * (1 - line)
+            
+            linePath.move(to: CGPoint(x: 0, y: yAxis))
+            linePath.addLine(to: CGPoint(x: size.width, y: yAxis))
+            
             linePath.lineWidth = 1.0
             linePath.stroke()
         }
